3bd1e090b9515b8ff997a65e6a1e85dc177af417
ProgrammerAuditore <victorvj098@gmail.com>
2021-11-04 13:14:37 -0600

    proyecto | react : Se agrego una tabla

--- app-react/src/pages/Studen.js
+++ app-react/src/pages/Studen.js
@@ -14,7 +14,31 @@ class Student extends Component {
                                 <Link to={'add-student'} className="btn btn-primary btn-sm float-end">Add Student</Link>
                             </div>
                             <div className="card-body">
-
+                                <table className="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Last Name</th>
+                                            <th>Email</th>
+                                            <th>Age</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Luis</td>
+                                            <td>Martinez</td>
+                                            <td>luis@example.com</td>
+                                            <td>52</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Carlos</td>
+                                            <td>Vallarta</td>
+                                            <td>carlos@example.com</td>
+                                            <td>34</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                
                             </div>
                         </div>
                     </div>
